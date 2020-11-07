c248c61184d3dd75a204e7940ab106d83052cae3
Qing Ma <gsmofgsm@hotmail.com>
2020-11-07 11:08:46 +0100

    create the main react component

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 // const express = require("express");
 import express from "express";
+import { readFileSync } from "fs";
 
 const app = new express();
+
+app.use(express.static("dist"));
 
 app.get("/", async (_req, res) => {
   // we are not going to use req, so
   // prepend with _ to let others know it is not used
-  res.send(`<h1>REACT IS EXCELLENT</h1>`);
+  //   res.send(`<h1>REACT IS EXCELLENT</h1>`);
+
+  const index = readFileSync(`public/index.html`, `utf-8`);
+  res.send(index);
 });
 
 app.listen(7777);
